adc8f0175b33ab1300fcb27a7b3847165a944cc0
JustinRowe15 <JustinRowe15@fullsail.edu>
2014-08-23 11:57:39 -0600

    Initial commit for AddictAid

---
+++ AddictAid/AddictAid/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AddictAid
+//
+//  Created by Justin Rowe on 8/23/14.
+//  Copyright (c) 2014 Justin Rowe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AddictAid/AddictAid/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AddictAid
+//
+//  Created by Justin Rowe on 8/23/14.
+//  Copyright (c) 2014 Justin Rowe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
